3ef59a556cc8191737289aee9bdacf38f7c08f00
Abora <https://github.com/abora97>
2020-07-17 22:12:05 +0200

    add api call

---
+++ app/src/main/java/com/abora/rxjavatest/data/PostInterface.java
@@ -0,0 +1,15 @@
+package com.abora.rxjavatest.data;
+
+
+
+import com.abora.rxjavatest.pojo.PostModel;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface PostInterface {
+    @GET("posts")
+    public Call<List<PostModel>> getPosts();
+}
