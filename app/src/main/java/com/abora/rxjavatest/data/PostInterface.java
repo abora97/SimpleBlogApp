b917e8b36ae043b27ce553f3123b2501592239bb
Abora <https://github.com/abora97>
2020-07-20 16:11:16 +0200

    finish app

--- app/src/main/java/com/abora/rxjavatest/data/PostInterface.java
+++ app/src/main/java/com/abora/rxjavatest/data/PostInterface.java
@@ -1,15 +1,18 @@
 package com.abora.rxjavatest.data;
 
 
-
 import com.abora.rxjavatest.pojo.PostModel;
 
 import java.util.List;
 
+import io.reactivex.Observable;
+import io.reactivex.Single;
 import retrofit2.Call;
 import retrofit2.http.GET;
 
 public interface PostInterface {
     @GET("posts")
-    public Call<List<PostModel>> getPosts();
+        //change from Call to observable
+        // reactivex
+    Single<List<PostModel>> getPosts();
 }
--- app/src/main/java/com/abora/rxjavatest/ui/main/PostViewModel.java
+++ app/src/main/java/com/abora/rxjavatest/ui/main/PostViewModel.java
@@ -1,5 +1,7 @@
 package com.abora.rxjavatest.ui.main;
 
+import android.util.Log;
+
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
@@ -9,6 +11,16 @@ import com.abora.rxjavatest.pojo.PostModel;
 
 import java.util.List;
 
+
+import io.reactivex.Observable;
+import io.reactivex.Observer;
+import io.reactivex.Single;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+
+
+import io.reactivex.schedulers.Schedulers;
+
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -16,18 +28,75 @@ import retrofit2.Response;
 public class PostViewModel extends ViewModel {
     MutableLiveData<List<PostModel>> postsMutableLiveData = new MutableLiveData<>();
     MutableLiveData<String> posts = new MutableLiveData<>();
+    private static final String TAG = "PostViewModel";
 
     public void getPosts() {
-        PostsClient.getINSTANCE().getPosts().enqueue(new Callback<List<PostModel>>() {
-            @Override
-            public void onResponse(Call<List<PostModel>> call, Response<List<PostModel>> response) {
-                postsMutableLiveData.setValue(response.body());
-            }
 
-            @Override
-            public void onFailure(Call<List<PostModel>> call, Throwable t) {
-                posts.setValue(t.getMessage());
-            }
-        });
+        // change from Observable to single
+        // data call oneTime will use single :D
+        Single<List<PostModel>> observable = PostsClient.getINSTANCE().getPosts()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread());
+
+        observable.subscribe(o -> postsMutableLiveData.setValue((o)), e -> Log.d(TAG, "getPosts: " + e));
+
+//        Observer<List<PostModel>> observer = new Observer<List<PostModel>>() {
+//            @Override
+//            public void onSubscribe(Disposable d) {
+//
+//            }
+//
+//            @Override
+//            public void onNext(List<PostModel> value) {
+//
+//                postsMutableLiveData.setValue(value);
+//            }
+//
+//            @Override
+//            public void onError(Throwable e) {
+//                posts.setValue(e.getMessage());
+//                Log.d(TAG, "onError: "+e);
+//            }
+//
+//            @Override
+//            public void onComplete() {
+//
+//            }
+//        };
+
+
+//        Observer observer=new Observer() {
+//            @Override
+//            public void onSubscribe(Disposable d) {
+//
+//            }
+//
+//            @Override
+//            public void onNext(Object value) {
+//               // postsMutableLiveData.postValue((List<PostModel>) value);
+//            }
+//
+//            @Override
+//            public void onError(Throwable e) {
+//
+//            }
+//
+//            @Override
+//            public void onComplete() {
+//
+//            }
+//        };
+
+//        PostsClient.getINSTANCE().getPosts().enqueue(new Callback<List<PostModel>>() {
+//            @Override
+//            public void onResponse(Call<List<PostModel>> call, Response<List<PostModel>> response) {
+//                postsMutableLiveData.setValue(response.body());
+//            }
+//
+//            @Override
+//            public void onFailure(Call<List<PostModel>> call, Throwable t) {
+//                posts.setValue(t.getMessage());
+//            }
+//        });
     }
 }
